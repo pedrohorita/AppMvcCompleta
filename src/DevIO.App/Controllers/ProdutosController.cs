9c7d6714f99e7abbc75711e02649b8a7bd818c95
Pedro R T A Horita <pedro_horita@hotmail.com>
2022-01-18 00:23:56 -0300

    Alteração da Controller Produto
    Alteração da controller produto para permitir upload de imagem

--- src/DevIO.App/Controllers/ProdutosController.cs
+++ src/DevIO.App/Controllers/ProdutosController.cs
@@ -2,9 +2,11 @@
 using DevIo.Business.Models;
 using DevIO.App.ViewModels;
 using DevIO.Business.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace DevIO.App.Controllers
@@ -58,12 +60,15 @@ namespace DevIO.App.Controllers
             if (!ModelState.IsValid)
                 return View(produtoViewModel);
 
+            var imgPrefixo = Guid.NewGuid() + "_";
+            if (!await UploadArquivo(produtoViewModel.ImagemUpload, imgPrefixo))
+                return View(produtoViewModel);
+
+            produtoViewModel.Imagem = imgPrefixo + produtoViewModel.ImagemUpload.FileName; 
+
             await _produtoRepository.Adicionar(_mapper.Map<Produto>(produtoViewModel));
 
-            return View(produtoViewModel);
-            
-            
-            
+            return RedirectToAction(nameof(Index));
         }
 
 
@@ -134,6 +139,25 @@ namespace DevIO.App.Controllers
         {            
             produto.Fornecedores = _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos());
             return produto;
+        }
+
+        private async Task<bool> UploadArquivo(IFormFile arquivo, string imgPrefixo)
+        {
+            if (arquivo.Length <= 0)
+                return false;
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/imagens", imgPrefixo + arquivo.FileName);
+
+            if(System.IO.File.Exists(path))
+            {
+                ModelState.AddModelError(string.Empty, "Já existe um arquivo com este nome!");
+                return false;
+            }
+
+            using var stream = new FileStream(path, FileMode.Create);
+            await arquivo.CopyToAsync(stream);
+
+            return true;
         }
     }
 }
